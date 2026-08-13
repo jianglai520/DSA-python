8593f9d14deae22d9bd4d2b14d024a37bf954b14
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-13 17:22:33 +0800

    fix: 更新twoSum(哈希表)

--- twoSum（哈希表）.py
+++ twoSum（哈希表）.py
@@ -1,13 +1,20 @@
-# 哈希表
+# 哈希表 注意：在python中，哈希表的实现就是字典和集合
+# 哈希表是一种通过key直接访问value的数据结构
 from typing import List
 
 def twosum(nums: List[int], target: int) -> List[int]:
     hashtable = {}
 
     for i, num in enumerate(nums):
-        if target - num in hashtable:
+        complement = target - num
+        if complement in hashtable:   # 检查键是否存在
             return [hashtable[target - num], i]
         hashtable[nums[i]] = i
     return []
 
-print(twosum([2, 7, 11, 15], 9))
+
+
+
+
+
+
