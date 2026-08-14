90830bda4a93d971a23ea43d010b5edd9b627835
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-15 00:08:06 +0800

    fix: 盛最多水的容器 -- 双指针（推荐）

---
+++ 力扣100/盛最多的容器（双指针）.py
@@ -0,0 +1,22 @@
+from typing import List
+
+def maxArea(height: List[int]) -> int:
+    left = 0
+    right = len(height) - 1
+    max_area = 0
+
+    while left < right:
+        width = right - left
+
+        current_height = min(height[left], height[right])
+        current_area = width * current_height
+
+        max_area = max(current_area, max_area)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_area
+
+print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
