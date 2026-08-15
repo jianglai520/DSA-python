7b96947ad73226bc84fe8abc0319d352402cbe31
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-15 12:04:19 +0800

    fix: 新增三数之和（双指针）-- 推荐

---
+++ 力扣100/三数之和（双指针）.py
@@ -0,0 +1,43 @@
+from typing import List
+
+def threeSum(nums: List[int]) -> int:
+    nums.sort()
+    result = []
+    n = len(nums)
+
+    for i in range(n - 2):
+        if nums[i] > 0:
+            break
+
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left = i + 1
+        right = n - 1
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+
+            if total == 0:
+                result.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+            elif total < 0:
+                left += 1
+            else:
+                right -= 1
+    return result
+
+
+# 测试
+print(threeSum([-1, 0, 1, 2, -1, -4]))  # [[-1, -1, 2], [-1, 0, 1]]
+print(threeSum([0, 0, 0]))              # [[0, 0, 0]]
+print(threeSum([0, 1, 1]))              # []
+
+
