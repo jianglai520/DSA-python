73e4b0bce8902cf307758a020d360752d39fcf85
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-18 14:04:50 +0800

    fix: 寻找重复数（双指针 + 链表）-- 推荐

---
+++ 力扣100/寻找重复数（快慢指针）.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def findDuplicate(nums: List[int]) -> int:
+    slow = nums[0]
+    fast = nums[0]
+
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+
+        if slow ==  fast:
+            break
+
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    return slow
+
+print(findDuplicate([1, 2, 4, 2, 2]))
