001f152b99b8a6e80d3dc0c1477d93748a8dd5d2
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-18 13:40:06 +0800

    fix: 寻找重复数（集合）-- 不推荐

---
+++ 力扣100/寻找重复数（集合）.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def findDuplicate(nums: List[int]) -> int:
+    set1 = set()
+    set2 = set()
+
+    for num in nums:
+        if num not in set1:
+            set1.add(num)
+        else:
+            set2.add(num)
+    return list(set2)[0]
+
+print(findDuplicate([1, 2, 4, 2, 2]))
