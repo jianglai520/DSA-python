52e2da0632cfc5b92971a13a4e5ed95c91190f77
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-18 15:25:19 +0800

    docs: update README.md

---
+++ 力扣100/轮转数组.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def rotate(nums: List[int], k: int) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    n = len(nums)
+    k = k % n
+    nums[:] = nums[-k:] + nums[:-k]
+
+nums1 = [1,2,3,4,5,6,7]
+rotate(nums1, 3)
+print(nums1)  # [5,6,7,1,2,3,4]
+
+nums2 = [-1,-100,3,99]
+rotate(nums2, 2)
+print(nums2)  # [3,99,-1,-100]
+
+nums3 = [1,2,3,4,5]
+rotate(nums3, 7)  # k 大于长度
+print(nums3)  # [4,5,1,2,3] 因为 7%5=2
