6304a1cd1d6f96ed97ea1bdb824212ecece91051
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-20 23:20:58 +0800

    fix: 相交链表（双指针）

---
+++ 力扣100/相交链表.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def getIntersectionNode(headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    if not headA or not headB:
+        return None
+
+    pA, pB = headA, headB
+
+    while pA != pB:
+        pA = pA.next if pA else headB
+        pB = pB.next if pB else headA
+
+    return pA
