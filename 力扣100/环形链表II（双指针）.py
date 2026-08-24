a79fb800a3b1ba8bcb4fd80a1553a84eb209de0c
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-25 00:39:48 +0800

    fix: 环形链表II（双指针）

---
+++ 力扣100/环形链表II（双指针）.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+
+def detectCycle(head: Optional[ListNode]) -> Optional[ListNode]:
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            break
+    else:
+        return None
+
+    slow = head
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+
+    return slow
