690e59eb025ff2b0dcaf4dd355c66d719d92a7e6
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-25 20:52:27 +0800

    fix: 将列表转换为链表

---
+++ 算法技巧/create_linked_list.py
@@ -0,0 +1,31 @@
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = None
+
+# 遍历链表
+def print_linked_list(head):
+    cur = head
+    while cur:
+        print(cur.val, end = " -> ")
+        cur = cur.next
+    print("None")
+
+
+# 将列表转换为链表
+def create_linked_list(arr):
+    if not arr:
+        return None
+
+    dummy = ListNode()
+    cur = dummy
+
+    for val in arr:
+        cur.next = ListNode(val)
+        cur = cur.next
+    return dummy.next
+
+arr = [1, 2, 3, 4]
+head = create_linked_list(arr)
+print_linked_list(head)
+
