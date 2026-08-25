aa71e62d243fad2d6f44e82906284ba4e2115047
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-25 22:27:21 +0800

    fix: 两两交换链表的节点（多指针 + 迭代）

---
+++ 力扣100/两两交换链表中的节点.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = None
+
+def swapPairs(head: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode(0, head)
+    prev = dummy
+
+    while prev.next and prev.next.next:
+        first = prev.next
+        second = prev.next.next
+
+        prev.next = second
+        first.next = second.next
+        second.next = first
+
+        prev = first
+
+    return dummy.next
