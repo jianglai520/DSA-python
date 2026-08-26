83dea9ea35b303f5a68f5ad709cb023343d3dcf9
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-26 17:54:57 +0800

    fix: 对称二叉树（递归）

---
+++ 力扣100/对称二叉树（递归）.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isSymmetric(root: Optional[TreeNode]) -> bool:
+    if not root:
+        return True
+
+    return isMirror(root.left, root.right)
+
+def isMirror(left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
+    if not left and not right:
+        return True
+
+    if not left or not right:
+        return False
+
+    return (left.val == right.val and isMirror(left.left, right.right) and isMirror(left.right, right.left))
