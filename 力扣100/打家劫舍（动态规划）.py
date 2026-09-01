ad876f78a0ad7de21d2b0f44de3947ebba02c2d6
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-01 21:57:57 +0800

    fix: 打家劫舍（动态规划）

--- 力扣100/打家劫舍（动态规划）.py
+++ 力扣100/打家劫舍（动态规划）.py
@@ -9,5 +9,12 @@ def rob(nums: List[int]) -> int:
 
     dp = [0] * (n + 1)
     dp[0] = 0
-    dp[1] nums[0]
+    dp[1] = nums[0]
+
+    for i in range(2, n + 1):
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+    return dp[n]
+
+
+
 
