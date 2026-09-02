abf33546843c0389c0e38de5cac52d2c0ba15e93
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-02 12:57:51 +0800

    fix: 单词拆分（动态规划）

---
+++ 力扣100/单词拆分（动态规划）.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def wordBreak(s: str, wordDict: List[str]) -> bool:
+    wordSet = set(wordDict)
+    n = len(s)
+
+    dp = [False] * (n + 1)
+    dp[0] = 0
+
+    for i in range(1, n+1):
+        for j in range(i):
+            if dp[j] and s[j:i] in wordSet:
+                dp[i] = True
+                break
+
+    return dp[n]
