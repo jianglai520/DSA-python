dfbace6a8ed0515b00c0b55d29fe733421be66ae
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-03 15:42:30 +0800

    fix: 最长递增子序列（动态规划）

---
+++ 力扣100/最长递增子序列（动态规划）.py
@@ -0,0 +1,13 @@
+def lengthOfLIS(nums: List[int]) -> int:
+    if not nums:
+        return 0
+
+    n = len(nums)
+    dp = [1] * n
+
+    for i in range(n):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
