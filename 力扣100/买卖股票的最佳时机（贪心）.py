a72eda09b2ac8d6b889dde6591602301c624be70
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-04 20:36:50 +0800

    fix: update 买卖股票的最佳时机（贪心）

--- 力扣100/买卖股票的最佳时机（贪心）.py
+++ 力扣100/买卖股票的最佳时机（贪心）.py
@@ -5,8 +5,10 @@ def maxProfit(prices: List[int]) -> int:
     max_profit = 0
 
     for price in prices:
-        if price < min_price:
-            min_price = price
-        elif price - min_price > max_profit:
-            max_profit = price - min_price
+        # if price < min_price:
+        #     min_price = price
+        # elif price - min_price > max_profit:
+        #     max_profit = price - min_price
+        min_price = min(min_price, price)
+        max_profit = max(max_profit, price - min_price)
     return max_profit
