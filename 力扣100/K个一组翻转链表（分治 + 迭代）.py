a8e95984456aaa41a74ba1a420d0480c0bccf9f1
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-08 22:01:40 +0800

    fix: update K个一组翻转链表（分治 + 指针 + 迭代）

--- 力扣100/K个一组翻转链表（分治 + 迭代）.py
+++ 力扣100/K个一组翻转链表（分治 + 迭代）.py
@@ -3,7 +3,7 @@ from typing import Optional
 class ListNode:
     def __init__(self, val = 0, next = None):
         self.val = val
-        self.next = None
+        self.next = next
 
 def reverseKGroup(head: Optional[ListNode], k: int) -> Optional[ListNode]:
     if not head or k == 1:
