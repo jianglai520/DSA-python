47c96bf5ed73be3a01075229bebe5edc0b0f1b08
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-08 15:24:16 +0800

    fix: 分割字符串（回溯）

---
+++ 力扣100/分割字符串（回溯）.py
@@ -0,0 +1,26 @@
+from typing import List
+
+def partition(s: str) -> List[List[str]]:
+    if not s:
+        return []
+
+    res = []
+    path = []
+
+    def is_palindrome(sub: str):
+        return sub == sub[::-1]
+
+    def backtrack(start: int):
+        if start == len(s):
+            res.append(path.copy())
+
+        for end in range(start, len(s)):
+            sub = s[start: end+1]
+
+            if is_palindrome(sub):
+                path.append(sub)
+                backtrack(end+1)
+                path.pop()
+
+    backtrack(0)
+    return res
