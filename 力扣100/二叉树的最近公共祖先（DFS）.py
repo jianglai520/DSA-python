cf9deb50a756dd00722e57098aae1e5f331ba97d
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-10 21:03:39 +0800

    fix: 二叉树的最近公共祖先（DFS）

---
+++ 力扣100/二叉树的最近公共祖先（DFS）.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if root is None or root == p or root == q:
+        return root
+
+    left = lowestCommonAncestor(root.left, p, q)
+    right = lowestCommonAncestor(root.right, p, q)
+
+    if left and right:
+        return root
+
+    return left if left else right
