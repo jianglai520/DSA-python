b78dac4d05926cc62766849f3a0e5f0e52b84b96
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-11 15:04:18 +0800

    fix: 课程表（拓扑排序）

---
+++ 力扣100/课程表（拓扑排序）.py
@@ -0,0 +1,26 @@
+from typing import List
+from collections import deque
+
+def canFinish(numCourses: int, prerequisites: List[List[int]]) -> bool:
+    graph = [[] for _ in numCourses]
+    indegree = [0] * numCourses
+
+    for a, b in prerequisites:
+        graph[b].append(a)
+        indegree[a] += 1
+
+    queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+
+    finished = 0
+    while queue:
+        course = queue.popleft()
+        finished += 1
+
+        for nxt in graph[course]:
+            indegree[nxt] -= 1
+            if indegree[nxt] == 0:
+                queue.append(nxt)
+
+    return finished == numCourses
+
+
