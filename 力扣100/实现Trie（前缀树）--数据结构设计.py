22274a5f05579a9b9cdafe983f9f60e70ec2c58a
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-11 16:14:00 +0800

    fix: 实现Trie（前缀树）--数据结构设计

---
+++ 力扣100/实现Trie（前缀树）--数据结构设计.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_end = True
+
+    def _find_node(self, prefix: str):
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return None
+            node = node.children[ch]
+        return None
+
+    def search(self, word: str) -> bool:
+        node = self._find_node(word)
+        return node is not None and node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        return self._find_node(prefix) is not None
+
+
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
