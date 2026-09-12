9e9bec7335d28feccfa82fe84506b0d7d679389a
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-12 16:53:24 +0800

    fix: 滑动窗口（暴力）

---
+++ 暑期学校（lzu)/滑动窗口（暴力）.py
@@ -0,0 +1,18 @@
+# 超时
+def main():
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    max_list = []
+    min_list = []
+
+    for i in range(n-k+1):
+        window = a[i:i+k]
+        max_list.append(max(window))
+        min_list.append(min(window))
+
+    print(" ".join(map(str, max_list)))
+    print(" ".join(map(str, min_list)))
+
+if __name__ == "__main__":
+    main()
