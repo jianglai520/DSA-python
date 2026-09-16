4655f2faef6498249ced75913689fc2eedad411f
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-16 16:11:26 +0800

    fix: update 课程表（拓扑排序）

--- 力扣100/课程表（拓扑排序）.py
+++ 力扣100/课程表（拓扑排序）.py
@@ -3,7 +3,7 @@ from collections import deque
 
 def canFinish(numCourses: int, prerequisites: List[List[int]]) -> bool:
     graph = [[] for _ in range(numCourses)]
-    indegree = [] * numCourses
+    indegree = [0] * numCourses
 
     for a, b in prerequisites:
         graph[b].append(a)
