f05ffec1c143b98635d9e22e8e1a6ef84ce6a895
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-16 20:26:21 +0800

    fix: 二叉树的直径

--- 力扣100/二叉树的直径.py
+++ 力扣100/二叉树的直径.py
@@ -10,13 +10,14 @@ def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
     max_diameter = 0
 
     def get_height(node):
+        nonlocal max_diameter
         if not node:
             return 0
 
         left_height = get_height(node.left)
         right_height = get_height(node.right)
 
-        max_diameter =  max(max_diameter, left_height + right_height)
+        max_diameter = max(max_diameter, left_height + right_height)
 
         return 1 + max(left_height, right_height)
     get_height(root)
