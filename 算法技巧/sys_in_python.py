7913ecb8441978379d65f40433a710f76f2d1bc1
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-17 12:50:12 +0800

    fix: update python中sys库的基本使用

--- 算法技巧/sys_in_python.py
+++ 算法技巧/sys_in_python.py
@@ -7,3 +7,10 @@ import sys
 
 data = sys.stdin.read().split()   # 如何结束：ctrl + D
 print(data)
+
+
+"""
+data = sys.stdin.buffer().read().split() 少一步解码，运行更快一点，结果和sys.stdin.read().split()完全一样
+
+sys.stdin.readline() 比input()快，但还是需要一行一行读
+"""
