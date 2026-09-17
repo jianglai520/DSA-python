58d893878758fa44a231b63f5b3352289ea3c92a
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-17 13:03:16 +0800

    docs: update python中sys的基本用法

--- 算法技巧/sys_in_python.py
+++ 算法技巧/sys_in_python.py
@@ -10,7 +10,7 @@ print(data)
 
 
 """
-data = sys.stdin.buffer().read().split() 少一步解码，运行更快一点，结果和sys.stdin.read().split()完全一样
+data = sys.stdin.buffer.read().split() 少一步解码，运行更快一点，结果和sys.stdin.read().split()完全一样
 
 sys.stdin.readline() 比input()快，但还是需要一行一行读
 """
